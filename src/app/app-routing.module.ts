9797c755d4e6effde7e316b601b44d1f434dc90a
saafi hayfa <saafihay109@gmail.com>
2022-03-15 14:06:08 +0100

    Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { MedicamentsComponent } from './medicaments/medicaments.component';
+import { Navbar1Component } from './navbar1/navbar1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "navbar", component : NavbarComponent},
+  {path: "login", component : LoginComponent},
+  {path: "medicaments", component : MedicamentsComponent},
+  {path: "navbar1", component : Navbar1Component},
+  { path: "", redirectTo: "", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { MedicamentsComponent } from './medicaments/medicaments.component';
+import { FormsModule } from '@angular/forms';
+import { Navbar1Component } from './navbar1/navbar1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    MedicamentsComponent,
+    Navbar1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
